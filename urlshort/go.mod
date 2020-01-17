7519033109f4fcae2b26c70a3f49884d189bdb95
Ryan Castner <castner.rr@gmail.com>
2020-01-17 09:25:43 -0500

    Add boltdb to urlshort exercise

--- urlshort/go.mod
+++ urlshort/go.mod
@@ -2,4 +2,8 @@ module github.com/audiolion/gophercises/urlshort
 
 go 1.13
 
-require gopkg.in/yaml.v2 v2.2.7
+require (
+	go.etcd.io/bbolt v1.3.3
+	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
+	gopkg.in/yaml.v2 v2.2.7
+)
--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -7,22 +7,37 @@ import (
 	"net/http"
 
 	"github.com/audiolion/gophercises/urlshort"
+	bolt "go.etcd.io/bbolt"
 )
 
 func main() {
 	var (
 		yamlFilename = flag.String("yaml", "urls.yaml", "yaml file of redirects")
 		jsonFilename = flag.String("json", "urls.json", "json file of redirects")
+		dbFilename   = flag.String("db", "urls.db", "boltdb file of redirects")
 	)
 	flag.Parse()
 
 	mux := defaultMux()
 
-	// Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	db, err := bolt.Open(*dbFilename, 0600, nil)
+	if err != nil {
+		fmt.Println("panic")
+		panic(err)
 	}
+	defer db.Close()
+
+	pathsToUrls, err := readRedirects(db)
+
+	if err != nil {
+		err = createDefaultRedirects(db)
+		pathsToUrls, err = readRedirects(db)
+	}
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
 	yamlHandler := makeHandler(yamlFilename, urlshort.YAMLHandler, mapHandler)
@@ -58,4 +73,51 @@ func makeHandler(filename *string, createHandler urlshort.Handler, fallback http
 		panic(err)
 	}
 	return handler
+}
+
+func readRedirects(db *bolt.DB) (map[string]string, error) {
+	pathsToUrls := make(map[string]string)
+
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("UrlBucket"))
+
+		if b == nil {
+			return fmt.Errorf("UrlBucket does not exist")
+		}
+
+		b.ForEach(func(k, v []byte) error {
+			var key []byte
+			var val []byte
+			copy(key, k)
+			copy(val, v)
+			pathsToUrls[string(key)] = string(val)
+			return nil
+		})
+
+		return nil
+	})
+
+	return pathsToUrls, err
+}
+func createDefaultRedirects(db *bolt.DB) error {
+	err := db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucket([]byte("UrlBucket"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+
+		pathsToUrls := map[string]string{
+			"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+			"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+		}
+		for k, v := range pathsToUrls {
+			err := b.Put([]byte(k), []byte(v))
+			if err != nil {
+				fmt.Printf("Error: putting %s, %s into bucket, %s\n", k, v, err)
+			}
+			fmt.Printf("Added key=%s, val=%s\n", k, v)
+		}
+		return nil
+	})
+	return err
 }
